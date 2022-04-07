2da60ba785c3964cd35003f67491a9edc11aa034
rajen <2019saurabh.borawake@ves.ac.in>
2022-04-07 20:18:53 +0530

    added notifications

--- service-worker.js
+++ service-worker.js
@@ -86,3 +86,57 @@ var urlsToCache = [
     )
     );
     });
+    self.addEventListener('activate' , ()=>{
+        console.log("sw is Activated")
+    })
+    
+    
+    // self.addEventListener('fetch', (event) => {
+    //   event.respondWith(
+    //     caches.match(event.request)
+    //       .then((response) => {
+    //         // The responce is in the cache
+    //         if (response) {
+    //           return response;
+    //         }
+    
+    //         // No cache match, we attempt to fetch it from the network
+    //         return fetch(event.request);
+    //       }
+    //     )
+    //   );
+    // });
+    
+    
+    self.addEventListener('push', e=> {
+    console.log('push', e);
+    var body;
+    
+    if (e.data) {
+    body = e.data.text();
+    } else {
+    body = 'Push message no payload';
+    }
+    var options = {
+    body: body,
+    icon: 'watch.jpg',
+    vibrate: [100, 50, 100],
+    data: {
+    dateOfArrival: Date.now(),
+    primaryKey: 1
+    },
+    actions: [
+    {action: 'explore', title: 'Explore this new world',
+    icon: 'shirt.jpg'},
+    {action: 'close', title: 'I don\'t want any of this',
+    icon: 'watch.jpg'},
+    ]
+    };
+    e.waitUntil(
+    self.registration.showNotification('Push Notification', options)
+    );
+    });
+    
+    self.addEventListener('sync', function(event) {
+        console.log("sync event", event);
+    });
